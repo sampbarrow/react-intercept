dd0ac03cf90480cd9560a17f15596b067d240682
Sam Barrow <sam@sambarrow.com>
2022-12-11 17:33:11 -0500

    .

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
-import { ComponentType } from "react"
+import { ComponentType, createElement } from "react"
 import { callOrGet, ValueOrFactory } from "value-or-factory"
 
+/**
+ * Returns a function that is applied to the next component.
+ */
 export function intercept<I extends {}, O extends {}>(mapper: (props: I) => ValueOrFactory<JSX.Element, [ComponentType<O>]>) {
     return (component: ComponentType<O>) => {
         return (props: I) => {
@@ -8,14 +11,10 @@ export function intercept<I extends {}, O extends {}>(mapper: (props: I) => Valu
         }
     }
 }
-/*
-export function pass<P extends {}>(mapper: (props: P) => ValueOrFactory<JSX.Element, [JSX.Element]>) {
-    return (component: ComponentType<P>) => {
-        return (props: P) => {
-            return callOrGet(mapper(props), createElement(component, props))
-        }
-    }
-}
+
+/**
+ * Transforms the incoming props and passes them to the next component.
+ */
 export function transform<I extends {}, O extends {}>(mapper: (props: I) => O) {
     return (component: ComponentType<O>) => {
         return (props: I) => {
@@ -23,11 +22,3 @@ export function transform<I extends {}, O extends {}>(mapper: (props: I) => O) {
         }
     }
 }
-export function discard() {
-    return (component: ComponentType<{}>) => {
-        return () => {
-            return createElement(component)
-        }
-    }
-}
-*/
