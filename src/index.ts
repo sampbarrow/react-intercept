82490b831233f8d09c2ed6cffa45618738f6ff5e
Sam Barrow <sam@sambarrow.com>
2023-02-20 21:25:22 -0500

    .

--- src/index.ts
+++ src/index.ts
@@ -18,3 +18,34 @@ export function transform<I extends {}, O extends {}>(mapper: (props: I) => O) {
         return (props: I) => createElement(component, mapper(props))
     }
 }
+
+type InterceptType<P> = {
+    type: "element",
+    element: ReactNode
+} | {
+    type: "transform",
+    props: P
+} | {
+    type: "custom",
+    function: (component: ComponentType<P>) => ReactNode
+}
+
+/**
+ * Transforms the incoming props and passes them to the next component.
+ */
+export function multicept<I extends {}, O extends {}>(mapper: (props: I) => ValueOrFactory<InterceptType<O>, [ComponentType<O>]>) {
+    return (component: ComponentType<O>) => {
+        return (props: I) => {
+            const result = callOrGet(mapper(props), component)
+            if (result.type === "element") {
+                return createElement(Fragment, { children: result.element })
+            }
+            else if (result.type === "transform") {
+                return createElement(component, result.props)
+            }
+            else {
+                return createElement(Fragment, { children: result.function(component) })
+            }
+        }
+    }
+}
